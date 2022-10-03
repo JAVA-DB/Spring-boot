86171d782e8ceb24192d41d9bc8d23066459d067
JAVA-DB <chlwoaud1234@gmail.com>
2022-10-03 21:09:54 +0900

    rm cached

---
+++ api/src/main/java/com/example/api/controller/PostController.java
@@ -0,0 +1,60 @@
+package com.example.api.controller;
+
+import com.example.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    @RequestMapping(value="/domain", method = RequestMethod.POST)
+    public String postExample(){
+        return "Hello Post API!!!";
+    }
+
+    @PostMapping(value = "/name")
+    public String getName(){
+        return "Flature";
+    }
+
+    @PostMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @PostMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    @PostMapping(value = "/variable3/{variable}")
+    public String getVariable3(@PathVariable(value = "variable") String var){
+        return var;
+    }
+
+    @PostMapping(value = "request1")
+    public String getRequestParam1(@RequestBody String name, @RequestBody String email, @RequestBody String organization){
+        return name + " " + email + " " + organization;
+    }
+
+    @PostMapping(value = "request2")
+    public String getRequestParam2(@RequestBody(required = true) String n, @RequestBody String email, @RequestBody String organization){
+        return n + " " + email + " " + organization;
+    }
+
+    @PostMapping(value = "request3")
+    public String getRequestParam3(@RequestBody Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    @PostMapping(value = "request4")
+    public String getRequestParam4(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
+    }
+}
