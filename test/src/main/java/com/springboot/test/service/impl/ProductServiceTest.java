c8379b51a385236038fa98a4bdd69213433ce524
JAVA-DB <chlwoaud1234@gmail.com>
2022-10-03 23:56:17 +0900

    테스트코드 예제 업데이트

--- test/src/main/java/com/springboot/test/service/impl/ProductServiceTest.java
+++ test/src/main/java/com/springboot/test/service/impl/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package com.springboot.test.service.impl;
 
+import com.springboot.test.data.dto.ProductDto;
 import com.springboot.test.data.dto.ProductResponseDto;
 import com.springboot.test.data.entity.Product;
 import com.springboot.test.data.repository.ProductRepository;
@@ -7,12 +8,22 @@ import com.springboot.test.data.service.impl.ProductServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
+
+@ExtendWith(SpringExtension.class)
+@Import(ProductServiceImpl.class)
 public class ProductServiceTest {
 
 //    private ProductRepository productRepository = Mockito.mock(ProductRepository.class);
@@ -44,7 +55,32 @@ public class ProductServiceTest {
 //        verify(productRepository).findById(123L);
 //
 //    }
-    private ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+//    private ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+//    private ProductServiceImpl productService;
+//
+//    @BeforeEach
+//    public void setUpTest(){
+//        productService = new ProductServiceImpl(productRepository);
+//    }
+//
+//    @Test
+//    void saveProductTest(){
+//        Mockito.when(productRepository.save(any(Product.class)))
+//                .then(returnsFirstArg());
+//
+//        ProductResponseDto productResponseDto = productService.saveProduct(new ProductDto("펜",1000,1234));
+//
+//        Assertions.assertEquals(productResponseDto.getName(), "펜");
+//        Assertions.assertEquals(productResponseDto.getPrice(), 1000);
+//        Assertions.assertEquals(productResponseDto.getStock(), 1234);
+//
+//        verify(productRepository).save(any());
+//
+//    }
+
+    @MockBean
+    private ProductRepository productRepository;
+    @Autowired
     private ProductServiceImpl productService;
 
     @BeforeEach
@@ -53,24 +89,17 @@ public class ProductServiceTest {
     }
 
     @Test
-    void getProductTest(){
-        Product givenProduct = new Product();
-        givenProduct.setNumber(123L);
-        givenProduct.setName("펜");
-        givenProduct.setPrice(1000);
-        givenProduct.setStock(1234);
+    void saveProductTest(){
+        Mockito.when(productRepository.save(any(Product.class)))
+                .then(returnsFirstArg());
 
-        Mockito.when(productRepository.findById(123L))
-                .thenReturn(Optional.of(givenProduct));
+        ProductResponseDto productResponseDto = productService.saveProduct(new ProductDto("펜",1000,1234));
 
-        ProductResponseDto productResponseDto = productService.getProduct(123L);
+        Assertions.assertEquals(productResponseDto.getName(), "펜");
+        Assertions.assertEquals(productResponseDto.getPrice(), 1000);
+        Assertions.assertEquals(productResponseDto.getStock(), 1234);
 
-        Assertions.assertEquals(productResponseDto.getNumber(), givenProduct.getNumber());
-        Assertions.assertEquals(productResponseDto.getName(), givenProduct.getName());
-        Assertions.assertEquals(productResponseDto.getPrice(), givenProduct.getPrice());
-        Assertions.assertEquals(productResponseDto.getStock(), givenProduct.getStock());
-
-        verify(productRepository).findById(123L);
+        verify(productRepository).save(any());
 
     }
 }
